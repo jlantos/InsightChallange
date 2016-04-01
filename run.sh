773e7473d1806cea90d688daefde0985246c92a5
Judit Lantos <lantosjudit@gmail.com>
2016-04-01 08:26:07 +0200

    run.sh added

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Calculate the average degree for the file in the input directory tweet_input 
+# and output the result file in the directory tweet_output
+python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
+
+
+

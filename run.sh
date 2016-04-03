23049abaa9f45ec4941c8332db42c7918b286c8e
Judit Lantos <lantosjudit@gmail.com>
2016-04-03 22:24:08 +0200

    Comments added

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Calculate the average degree for the file in the input directory tweet_input 
-# and output the result file in the directory tweet_output
+# Calculate the average vertex degree for each line of the file in the input
+# directory tweet_input and output the result file in the directory tweet_output
 python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
 
 
